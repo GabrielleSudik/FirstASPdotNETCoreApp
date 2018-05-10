a269ea12f6d56aaf2fa11734c39b5b6f9acaa031
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-05-10 14:08:37 -0400

    final commit, lesson complete

--- FirstASPdotNETCoreApp/Models/Employee.cs
+++ FirstASPdotNETCoreApp/Models/Employee.cs
@@ -12,3 +12,12 @@ namespace FirstASPdotNETCoreApp.Models
         //prop tab tab
     }
 }
+
+//instructor didn't say so, but I guess this is a code-first DB model.
+//it is.
+
+//at the end of the class he walked through how to comment to a data-first model.
+//but (1) my VS wasn't installing something and (2) I didn't have a db ready to go.
+//so I just watched.
+
+//you saved a copy of the pp slides if you want to try another time.
